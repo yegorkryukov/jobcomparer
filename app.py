e2dc29c8045425f3913035f92a943442dc5c239f
Yegor Kryukov <yegor.kryukov@gmail.com>
2020-05-31 15:49:32 -0400

    Running template

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/<string:name>")
-def index(name):
+job_descriptions = [
+  {
+  "title": "Senior Data Scientist", 
+  "company": "Jobot", 
+  "description": "This Jobot Job is hosted by: Ellina Oganyan Are you a fit? Easy Apply now by clicking the \"Apply on compan"
+  }
+]
+
+@app.route('/')
+@app.route('/home')
+def index():
   return render_template(
-    'index.html', name=name)
+    'index.html', 
+    job_descriptions=job_descriptions)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
